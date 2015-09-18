7eea6272101c6bf8e1983a928524db22e3ba6ca3
弥超 <tony_mistark@michaodeAir.lan>
2015-09-18 21:26:21 +0800

    Initial Commit

---
+++ Student/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Student
+//
+//  Created by 弥超 on 15/9/18.
+//  Copyright (c) 2015年 弥超. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
